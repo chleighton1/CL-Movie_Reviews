3d7266e801af815383efb02e104df9ae3f8848e3
Charles Leighton <chleighton1@gmail.com>
2023-08-24 14:45:36 -0400

    Complete submitReview function

--- notion.js
+++ notion.js
@@ -3,18 +3,47 @@ const { response } = require("express");
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-function submitReview({ title }) {
+async function getGenres() {
+  const database = await notion.databases.retrieve({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+
+  console.log(database.properties.Genre.multi_select);
+}
+
+function submitReview({ movieTitle, genre, review, rating }) {
   notion.pages.create({
     parent: {
       database_id: process.env.NOTION_DATABASE_ID,
     },
     properties: {
+      Genre: {
+        multi_select: [
+          {
+            name: genre,
+          },
+        ],
+      },
+      Review: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: review,
+            },
+          },
+        ],
+      },
+      Rating: {
+        number: rating,
+      },
       "Movie Title": {
+        type: "title",
         title: [
           {
             type: "text",
             text: {
-              content: title,
+              content: movieTitle,
             },
           },
         ],
@@ -23,4 +52,12 @@ function submitReview({ title }) {
   });
 }
 
-submitReview({ title: "Test num 2" });
+getGenres();
+
+// submitReview({
+//   movieTitle: "Barbie",
+//   genre: "Comedy",
+//   review:
+//     "I liked it. It was fun and a great commentary on modern gender roles",
+//   rating: 7.6,
+// });
